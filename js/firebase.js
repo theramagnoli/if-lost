026ba9e57c5197e32f35db60f427c586754e95ff
Sergio Ramagnoli <sergio.rodriguezrm@uanl.edu.mx>
2022-04-27 18:39:50 -0500

    ps le agregué cosas

--- js/firebase.js
+++ js/firebase.js
@@ -1,54 +1,93 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword,
+  signInWithEmailAndPassword, updateProfile, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore"; 
 
-    //crear cuentas
-    const formCrearCuenta = document.querySelector("#crearCuenta");
-    formCrearCuenta.addEventListener("submit", (e) => {
-      e.preventDefault();
-      createUserWithEmailAndPassword(
-        auth,
-        formCrearCuenta.correo.value,
-        formCrearCuenta.contraseña.value
-      )
-        .then((userCredential) => {
-          const user = userCredential.user;
-          console.log(user);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    });
+const firebaseConfig = {
+  apiKey: "AIzaSyCO-1zlqihZNJAGUqfbezRKdAbx2xVRurA",
+  authDomain: "if-lost-159f6.firebaseapp.com",
+  projectId: "if-lost-159f6",
+  storageBucket: "if-lost-159f6.appspot.com",
+  messagingSenderId: "55254413607",
+  appId: "1:55254413607:web:2b14450ddb1a97bdccb902"
+};
 
-    //iniciar sesión
-    const botonInicarSesion = document.querySelector('#botonInicio');
-    botonInicarSesion.addEventListener('click', function(e){
-      const formIniciarSesion = document.querySelector("#iniciarSesión");
-      formIniciarSesion.addEventListener("submit", (e) => {
-        e.preventDefault();
-        signInWithEmailAndPassword(
-          auth,
-          formIniciarSesion.correo.value,
-          formIniciarSesion.contraseña.value
-        )
-          .then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user);
-          })
-          .catch((error) => {
-            console.log(error.message);
-          });
-      });
 
-    
-    // obtener más información
-    const botonEntrar = document.querySelector('#entrar');
-    botonEntrar.addEventListener('submit', function(e){
-      updateProfile(auth.currentUser, {
-        displayName: "Sergio Ramagnoli",
-        photoURL: "https://i.pinimg.com/474x/5b/26/14/5b261453f19c9badc3fc053a200eb4b2.jpg"
-      }).then((userCredential) => {
-        console.log(userCredential.user)
-      }).catch((error) => {
-        console.log(error.message)
-      });
-    });
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const  database = getFirestore();
+
+//crear cuentas
+const formCrearCuenta = document.querySelector("#crearCuenta");
+formCrearCuenta.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createUserWithEmailAndPassword(
+    auth,
+    formCrearCuenta.correo.value,
+    formCrearCuenta.contraseña.value
+  )
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
     })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  // obtener info personal (nombre, etc.)
+  const formActualizarInfo = document.querySelector('#informaciónPersonal');
+  formActualizarInfo.addEventListener('submit', function(e){
+    e.preventDefault();
+    let nombre = formActualizarInfo.nombres.value + " " + formActualizarInfo.apellidos.value;
+    let numero = formActualizarInfo.numero.value;
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setDoc(doc(database, "infoUsuarios", user.uid), {
+          numero: formActualizarInfo.numero.value,
+          nombres: formActualizarInfo.nombres.value,
+          apellidos: formActualizarInfo.apellidos.value,
+          ciudad: formActualizarInfo.ciudad.value
+        });
+      }
+      else {
+        console.log("error")
+      }
+    });
+
+    updateProfile(auth.currentUser, {
+      displayName: nombre,
+      phoneNumber: numero,
+    }).then(() => {
+      console.log("perfil actualizado")
+    }).catch((error) => {
+      console.log(error.message)
+    });
+  });
+});
+
+//iniciar sesión
+const botonInicarSesion = document.querySelector('#botonInicio');
+botonInicarSesion.addEventListener('click', function(e){
+  const formIniciarSesion = document.querySelector("#iniciarSesión");
+  formIniciarSesion.addEventListener("submit", (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(
+      auth,
+      formIniciarSesion.correo.value,
+      formIniciarSesion.contraseña.value
+    )
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  });
+})
+
+//FIRESTORE
+
+// Add a new document in collection "cities"
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,21 +1,13 @@
 <template>
   <img src="images\logonegro.png" alt="if lost" class="logo" />
-  <div v-if="iniciarSesión == 0">
-    <div>
-      <h1 class="vertical-center">Bienvenido</h1>
-    </div>
-     <h2>Crea una cuenta para comenzar</h2>
+  <div v-if="iniciarSesión == 0 && obtenerInfo == 0">
+    <h1 class="vertical-center">Bienvenido</h1>
+    <h2>Crea una cuenta para comenzar</h2>
 
-    <form id="crearCuenta">
-      <div>
-        <input type="email" name="correo" placeholder="Correo electrónico" />
-      </div>
-
-      <div>
-        <input type="password" name="contraseña" placeholder="Contraseña" />
-      </div>
-
-      <button type="submit">Crear cuenta</button>
+    <form id="crearCuenta" v-on:submit="obtenerInfo = 1">
+      <input type="email" name="correo" placeholder="Tu correo electrónico" />
+      <input type="password" name="contraseña" placeholder="Una buena contraseña" />
+      <button type="submit">Crear mi cuenta</button>
     </form>
 
     <div>
@@ -28,30 +20,51 @@
     </div>
   </div>
 
+  <div v-if="obtenerInfo == 1">
+    <h1>Ayúdanos a ayudarte</h1>
+    <h2>Queremos conocerte mejor</h2>
+    <form id="informaciónPersonal"  v-on:submit="obtenerInfo = 2">
+      <input type="text" name="nombres" placeholder="Tu nombre(s)" />
+      <input type="text" name="apellidos" placeholder="Tu apellido(s)" />
+      <input type="tel" name="numero" placeholder="Tu número de teléfono" />
+      <input type="text" name="ciudad" placeholder="Ciudad donde vives">
+      <button type="submit">Continuar</button>
+    </form>
+  </div>
+
+  <div v-if="obtenerInfo == 2">
+    <h1>A ver tu cara, mijo</h1>
+    <h2>Una foto puede ayudar a reconocerte, procura que sea clara y sin filtros</h2>
+
+    <div>
+      <img src="images\perfildef.webp" alt="perfil" class="subir-perfil" />
+    </div>
+
+    <form id="fotoPerfil">
+      <input type="file" name="foto" accept="image/png, image/jpeg, image/jpg"/>
+      <button type="submit">Subir mi foto</button>
+      <button>Omitir</button>
+    </form>
+  </div>
+
   <!-- sección de inicio de sesión -->
   <div v-if="iniciarSesión == 1">
     <div>
       <h1 class="vertical-center">Bienvenido de nuevo</h1>
     </div>
-     <h2>Nos alegra verte otra vez</h2>
+    <h2>Nos alegra verte otra vez</h2>
 
     <form id="iniciarSesión">
-      <div>
         <input type="email" name="correo" placeholder="Correo electrónico" />
-      </div>
-
-      <div>
         <input type="password" name="contraseña" placeholder="Contraseña" />
-      </div>
-
-      <button type="submit" id="entrar">Entrar</button>
+      <button type="submit" id="entrar">Entrar a mi cuenta</button>
     </form>
 
     <div>
       <h3>
         ¿Aún no tienes cuenta?
         <a v-on:click="iniciarSesión = 0" id="botonCrearCuenta" href="#"
-          >Crear una</a
+          >Crea una</a
         >
       </h3>
     </div>
@@ -59,72 +72,32 @@
 </template>
 
 <script>
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile} from "firebase/auth";
-
 export default {
   data() {
     return {
       iniciarSesión: 0,
+      obtenerInfo: 0,
     };
   },
   mounted() {
-    const auth = getAuth(app);
-
-    //crear cuentas
-    const formCrearCuenta = document.querySelector("#crearCuenta");
-    formCrearCuenta.addEventListener("submit", (e) => {
-      e.preventDefault();
-      createUserWithEmailAndPassword(
-        auth,
-        formCrearCuenta.correo.value,
-        formCrearCuenta.contraseña.value
-      )
-        .then((userCredential) => {
-          const user = userCredential.user;
-          console.log(user);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    });
-
-    //iniciar sesión
-    const botonInicarSesion = document.querySelector('#botonInicio');
-    botonInicarSesion.addEventListener('click', function(e){
-      const formIniciarSesion = document.querySelector("#iniciarSesión");
-      formIniciarSesion.addEventListener("submit", (e) => {
-        e.preventDefault();
-        signInWithEmailAndPassword(
-          auth,
-          formIniciarSesion.correo.value,
-          formIniciarSesion.contraseña.value
-        )
-          .then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user);
-          })
-          .catch((error) => {
-            console.log(error.message);
-          });
-      });
-
-    
-    // obtener más información
-    const botonEntrar = document.querySelector('#entrar');
-    botonEntrar.addEventListener('submit', function(e){
-      updateProfile(auth.currentUser, {
-        displayName: "Sergio Ramagnoli",
-        photoURL: "https://i.pinimg.com/474x/5b/26/14/5b261453f19c9badc3fc053a200eb4b2.jpg"
-      }).then((userCredential) => {
-        console.log(userCredential.user)
-      }).catch((error) => {
-        console.log(error.message)
-      });
-    });
-    })
   },
-  computed: {},
+  computed: {
+    perfilActualizado(){
+      let perfil = document.querySelector("#fotoPerfil");
+      let fotoPerfil = perfil.foto;
+      if(fotoPerfil.value){
+        console.log("perfil")
+      }
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.subir-perfil {
+  width: 200px;
+  margin: auto;
+  display: block;
+  border-radius: 50%;
+}
+</style>
