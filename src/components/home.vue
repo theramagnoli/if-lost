18ef323bb71e179241aa9fbb1e99fda26c130347
Alejandro369159 <alejandro.sauqueprz@uanl.edu.mx>
2022-08-10 10:43:58 -0500

    SetDoc de Location
    Estoy haciendo el proceso de currentlocation y ponerlo en una alerta de firebase.

--- src/components/home.vue
+++ src/components/home.vue
@@ -18,8 +18,16 @@
 </template>
 
 <script>
-import { auth, onAuthStateChanged } from "/js/firebase.js";
+import { mapState } from "vuex";
+import { auth, onAuthStateChanged, database, setDoc, doc } from "/js/firebase.js";
 export default {
+  data() {
+    return {
+      timestamp: 0,
+      link: "",
+      mapa: [],
+    }
+  },
   methods: {
     obtenerUsuario() {
       onAuthStateChanged(auth, (user) => {
@@ -30,27 +38,58 @@ export default {
       });
     },
     viajar() {},
-    obtenerUbicacion() {
+    async obtenerUbicacion() {
       // se está probando la opción de enviar la alerta en el momento con la ubicación actual
       // pero en caso de consolidarse, no se estaría ejecutando en este componente
-      navigator.geolocation.getCurrentPosition(
-        (ubi) =>
-          console.log(
-            // de obtener la ubicacion, la devuelve en Google Maps
-            `https://www.google.com/maps/@${ubi.coords.latitude},${ubi.coords.longitude},20z`
-          ),
-        (err) => console.log(err), // de haber un error, lo devuelve
-        {
-          // configuración
+      const Functionsuccess = ((ubi) => {
+        console.log(ubi)
+        console.log(`https://www.google.com/maps/@${ubi.coords.latitude},${ubi.coords.longitude},20z`)
+        this.timestamp = ubi.timestamp
+        this.link = `https://www.google.com/maps/@${ubi.coords.latitude},${ubi.coords.longitude},20z`
+        this.mapa = [ubi.coords.latitude, ubi.coords.longitude];
+      })
+      function error(err){
+        console.log(err);
+      }
+      var options = {
+         // configuración
           enableHighAccuracy: true, // alta precisión
           maximumAge: 0, // no obtener cache
           timeout: 5000, // tiempo máximo a esperar
+      };
+      function getLocation(){
+        if(navigator.geolocation){
+          navigator.geolocation.getCurrentPosition(Functionsuccess, error, options)
         }
-      );
+        else{
+          alert('Location no disponible')
+        }
+      }
+      // Proceso de SetDoc
+      let docRef = await doc(database, "alertas", this.usuario.viaje.clave)
+      let data = {
+        clave: this.usuario.viaje.clave,
+        contacto: {
+          correo: this.usuario.contacto.correo,
+          tel: this.usuario.contacto.tel,
+        },
+        creador: this.usuario.nombre,
+        horaEnvio: this.timestamp,
+        link: this.link,
+        mapa: this.mapa,
+      }
+      await setDoc(docRef, data) .then(() => console.log(data)) .catch(err => {
+        console.log(err);
+      })
     },
   },
   mounted() {
     this.obtenerUsuario();
+  },
+  computed: {
+    usuario() {
+      return this.$store.getters.usuario;
+    }
   },
 };
 </script>
